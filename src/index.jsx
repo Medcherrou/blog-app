0a2777e27f52b562a6d905c41ce92f5ae5b82e2a
Mohamed Cherrou <101065974+Medcherrou@users.noreply.github.com>
2025-01-18 11:34:40 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { createStore , applyMiddleware,compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import App from './components/App'
 import reducers from './reducers'
 
-
-const composeEnhanced = compose(applyMiddleware(thunk),composeWithDevTools())
-
-const store = createStore(reducers,composeEnhanced)
+// Use composeWithDevTools directly with applyMiddleware
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-	</React.StrictMode>
+  </React.StrictMode>
 )
